d6da766e9669b6a80b4594a05a57b7a3139b19fe
Raphael Americano <raphael@raphaelamericano.com.br>
2020-10-18 01:21:44 -0300

    Adicionando o filtro CSRF

--- src/main/java/com/zup/zupbank/configuration/WebSecurityConfig.java
+++ src/main/java/com/zup/zupbank/configuration/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfFilter;
 
 
 @Configuration
@@ -12,6 +13,7 @@ public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws  Exception {
         http
                 .csrf().disable()
+//                .addFilterAfter(new CsrfLoggerFilter(), CsrfFilter.class)
                 .authorizeRequests()
                 //Permite acesso a rota sem autenticacao
                 .antMatchers("/conta/nova", "/conta", "/auth/csrf").permitAll()
