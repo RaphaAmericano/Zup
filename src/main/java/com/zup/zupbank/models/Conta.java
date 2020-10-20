9774dbcb960a0b73d999bbfdb8c0fb23eef2cba5
Raphael Americano <raphael@raphaelamericano.com.br>
2020-10-20 10:23:26 -0300

    Adicionando atributos na classe de Conta

--- src/main/java/com/zup/zupbank/models/Conta.java
+++ src/main/java/com/zup/zupbank/models/Conta.java
@@ -8,6 +8,11 @@ public class Conta {
     protected Endereco endereco;
     protected byte[] foto_cpf;
 
+    private int agencia;
+    private long numeroConta;
+    private int codigoBanco;
+    private double saldo;
+
     public Pessoa getPessoa() {
         return pessoa;
     }
@@ -30,5 +35,46 @@ public class Conta {
 
     public void setFoto_cpf(byte[] foto_cpf) {
         this.foto_cpf = foto_cpf;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        if( agencia > 9999){
+            throw new IllegalArgumentException("Agência deve ser menor que 4 dígitos");
+        }
+        this.agencia = agencia;
+    }
+
+    public long getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(long numeroConta) {
+        if( numeroConta > 99999999L){
+            throw new IllegalArgumentException("Agência deve ser menor que 8 dígitos");
+        }
+        this.numeroConta = numeroConta;
+    }
+
+    public int getCodigoBanco() {
+        return codigoBanco;
+    }
+
+    public void setCodigoBanco(int codigoBanco) {
+        if( codigoBanco > 999){
+            throw new IllegalArgumentException("Agência deve ser menor que 4 dígitos");
+        }
+        this.codigoBanco = codigoBanco;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
     }
 }
